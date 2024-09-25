4646aabede7631f5eea28a822454139f85c49b2a
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-09-24 19:53:37 -0600

    Pr3_Trabajo_en_clase

---
+++ Pr3_Trabajo_en_clase.py
@@ -0,0 +1,13 @@
+print(" ")
+
+print("Edgar Gael Garcia Camacho :Pr3_Trabajo_en_clase")
+
+print(" ")
+
+km=int(input("Ingresa la cantidad de kilometros recorridos por una motocicleta :"))#Te pide ingresar la cantidad de kilometros recorridos
+
+lt=int(input("Ingresa la cantidad de combustible consumido durante el recorrido :"))#Te pide ingresar la cantidad de combustible consumido
+
+r= (lt * km /1000)#La operacion para realizar el combustible por kilometro.
+
+print("El consumo de combustible por kilometro es :", r ,"litros")#Te saltara este mensaje con el resultado
